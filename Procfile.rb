9c6c792c72a27563bf6633013ca9af664f899b68
Tom Bombadil <amanibhavam@destructuring.org>
2011-12-26 10:38:18 +0000

    procfile: add hostess

--- Procfile.rb
+++ Procfile.rb
@@ -1,8 +1,10 @@
 ENV['RTFM_REPOS'] ||= "#{Dir.pwd}:/"
 ENV['RTFMd_WIKI'] ||= Dir.pwd
 ENV['RTFMd_BASE_PATH'] ||= "/"
-rtfm_ru = %x(bundle show RTFMd).strip
+rtfm = %x(bundle show RTFMd).strip
+private_event = %x(bundle show private_event).strip
 <<HERE
-rtfm: thin start -R #{rtfm_ru}/config/rtfmd.ru
+rtfm: thin start -R #{rtfm}/config/rtfmd.ru
+hostess: thin start -R #{private_event}/config/hostess.ru
 git: git daemon --reuseaddr --verbose  --base-path=. --export-all ./.git
 HERE
