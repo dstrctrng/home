241f8f679e700384ae9ca26f1075dce2dd31e663
Tom Bombadil <amanibhavam@destructuring.org>
2012-01-21 05:50:19 +0000

    pods alpha omega

---
+++ sorted.rb
@@ -0,0 +1,29 @@
+class Hash
+  alias old_keys keys
+
+  def keys
+    pinned = %w(run_list cap_group pods)
+
+    old_keys.sort {|a,b|
+      ia = pinned.index a
+      ib = pinned.index b
+
+      if ia
+        ib ? ia <=> ib : -1
+      else
+        ib ? 1         : a.to_s <=> b.to_s
+      end
+    }
+  end
+
+  def each
+    keys.each do |k|
+      yield k, self[k]
+    end
+  end
+end
+
+require 'yaml'
+require 'json'
+
+puts JSON.load(File.read(ARGV[0])).to_yaml
