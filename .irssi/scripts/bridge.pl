8f12675db3cf1f0810526dacf1f6fb3a1e4ae74c
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-31 02:11:36 -0700

    znc auto-op: getting opped

--- .irssi/scripts/bridge.pl
+++ .irssi/scripts/bridge.pl
@@ -1,6 +1,16 @@
-sub server_incoming {
-  my ($server, $data) = @_;
-  print "server_incoming: $data"
+use strict;
+use Digest::MD5 qw(md5_hex);
+
+my ($password) = "";
+
+sub notice {
+  my ($server, $msg, $nick, $address, $target) = @_;
+
+  my ($challenge) = $msg =~ m{^\S+\s+:!ZNCAO\s+CHALLENGE\s+(.*)$};
+  my ($response) = md5_hex($password . "::" . $challenge);
+
+  Irssi::active_win->print("znc auto-op challenge: $msg, $nick, $address");
+  $server->command("NOTICE $nick !ZNCAO RESPONSE $response");
 }
 
-Irssi::signal_add("server incoming", "server_incoming")
+Irssi::signal_add("event notice", "notice");
