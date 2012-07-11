6f711438451c18118d6f7718e00b75aa173070fc
Tom Bombadil <amanibhavam@destructuring.org>
2012-07-11 13:51:27 -0700

    interpolated $microwave_root wrong

--- cookbooks/example/recipes/default.rb
+++ cookbooks/example/recipes/default.rb
@@ -2,7 +2,7 @@ execute "example" do
   command "uname -a"
 end
 
-template "{$microwave_root}/config/example.yml" do
+template "#{$microwave_root}/config/example.yml" do
   mode 0644
   source "example.erb"
 end
