379dd8b1770a3e4c7011bd79a1c48da9df6461bc
Tom Bombadil <amanibhavam@destructuring.org>
2012-07-17 04:38:34 -0700

    every dir is $HOME to this app

--- nodes/localhost.rb
+++ nodes/localhost.rb
@@ -2,9 +2,8 @@ run_list [ 'role[shell]' ]
 platform "generic"
 platform_version "0.0.1"
 
-release_dir "#{ENV['PWD']}"
-project_dir "#{ENV['PWD']}"
-gconfig_dir "#{ENV['PWD']}/config/development"
+release_dir "#{ENV['HOME']}"
+project_dir "#{ENV['HOME']}"
 home_dir ENV['HOME']
 app_env "development"
 
