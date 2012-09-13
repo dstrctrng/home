aacf8d0cd83eb46624a3d47df26f47fff9eb88d5
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-13 15:10:04 -0700

    localdomain is not an environment, its a podname, so use development to mean localdomain; vagrants are master env

--- nodes/staging.rb
+++ nodes/staging.rb
@@ -2,8 +2,7 @@ run_list []
 platform "generic"
 platform_version "0.0.1"
 
-release_dir "#{ENV['PWD']}"
+release_dir ENV['PWD']
 project_dir "XXX"
-gconfig_dir "XXX"
 home_dir "XXX"
 app_env "staging"
