97763edb7de8fe133641a16652db98329d18e0c7
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-19 00:12:12 -0700

    agent runit service

--- cookbooks/badonkadonk/recipes/default.rb
+++ cookbooks/badonkadonk/recipes/default.rb
@@ -1,3 +1,7 @@
-directory "#{node[:release_dir]}/log" do
-  mode 0755
+directory "#{node[:release_dir]}/log"
+directory "#{node[:release_dir]}/service"
+
+runit_service "agent" do
+  project_dir node[:project_dir]
+  release_dir node[:release_dir]
 end
