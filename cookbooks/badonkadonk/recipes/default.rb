28ce0d11b690fef45496a7a7ee70749ce2efd8aa
Tom Bombadil <amanibhavam@destructuring.org>
2012-10-14 00:39:54 -0700

    no dummy agent service

--- cookbooks/badonkadonk/recipes/default.rb
+++ cookbooks/badonkadonk/recipes/default.rb
@@ -1,11 +1,6 @@
 directory "#{node[:release_dir]}/log"
 directory "#{node[:release_dir]}/service"
 
-runit_service "agent" do
-  project_dir node[:project_dir]
-  release_dir node[:release_dir]
-end
-
 runit_service "bitlbee" do
   project_dir node[:project_dir]
   release_dir node[:release_dir]
