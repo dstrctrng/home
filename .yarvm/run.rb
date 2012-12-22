2a972c4d456e1bf4db23965a05696564410e8be0
Tom Bombadil <amanibhavam@destructuring.org>
2012-12-22 13:29:54 -0800

    replace rbenv and rvm with yarvm, easy to swap out combinations of rvm.rbenv

--- .yarvm/run.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby-local-exec
-
-puts "version #{RUBY_VERSION}, platform #{RUBY_PLATFORM}"
--- .yarvm/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ruby="$1"; shift
-mkdir -p $ruby
-cd $ruby
-rbenv local $ruby
-ruby ../run.rb
